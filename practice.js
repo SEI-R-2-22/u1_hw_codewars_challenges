90ea26c6e1a240a35477ceaca866cabe046f1562
Job85 <julianjernigan@ymail.com>
2022-03-02 08:52:51 -0500

    saved js

--- practice.js
+++ practice.js
@@ -1,15 +1,16 @@
 // const arr = [1, -4, -3, -8, 10]
-// const positiveNumbers = arr.filter(n => n > 0);
-// let sum = 0
-// for (let i = 0; i < positiveNumbers.length; i++) {
-//     sum += positiveNumbers[i];
+// // const positiveNumbers = arr.filter(n => n > 0);
+// // let sum = 0
+// // for (let i = 0; i < positiveNumbers.length; i++) {
+// //     sum += positiveNumbers[i];
+// // }
+// function sum (numbers) {
+// let arr = 0
+// for (let i = 0; i < numbers.length; i +=1){
+//   arr += numbers[i]
 // }
-// console.log(sum);
-// let a = 4;
-// let b = 1;
-// () => a + b
-  
-  (2,2) => a ** b;
-  console.log(a ** b)
-
-
+//     console.log(arr)
+//   return arr
+    
+// }
+// sum([1, 3, -5, 9]);
