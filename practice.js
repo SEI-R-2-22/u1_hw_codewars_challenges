9c9d4cf1da52d2c745169e3b557322ec9d03978f
Jean Garcia <jeangar92@gmail.com>
2022-03-01 22:10:42 -0500

    updated solution.md with my answers

---
+++ practice.js
@@ -0,0 +1,94 @@
+// Kata.MakeNegative(1);  // return -1
+
+// function chaToNeg(num)
+// {
+//     return -Math.abs(num);
+// }
+// console.log(chaToNeg(1))
+
+// // Kata.MakeNegative(-5); // return -5
+// // Kata.MakeNegative(0);  // return 0
+
+
+
+// let numArr = [1,-4,7,12];
+// let total = 0
+
+// for (let i = 0; i < numArr.length; i++){
+//     if(numArr[i] > 0){
+//         total += numArr[i];
+//     }
+//   }
+//   console.log(total)
+
+// Now you have to write a function that takes an argument and returns the square of it.
+
+
+
+// function sqrRoot() {
+//     return Math.sqrt(64)
+// }
+
+// console.log(sqrRoot())
+
+// swrFunction = (num) => { let result = 10 * 10; 
+//     return result
+// }
+// console.log(result)
+
+
+//   function sumArr(posArr)
+  
+//   let total = 0;
+  
+//   for(let number of posArr){
+//       total += posArr
+//   }
+
+// return total;
+
+
+
+// let arr = [2,4,6,8]
+
+// for (let i = 0; i < numArr.length; i++){
+//     if(numArr[i] > 0){
+//         total += numArr[i];
+//     }
+//   }
+//   console.log(total)
+
+
+
+
+// let arr = [2,4,6,8]
+
+// function arrSum {
+
+// }
+
+
+// let total = 0;
+// function arrPlus (){
+//     return total += numArr[i];
+// }
+// console.log(arrPlus([2,4,6]))
+// arrPlus([2,4,6])
+
+// const array = [2,4,6];
+// let sum = 0;
+
+// for (let i = 0; i < array.length; i++) {
+//     sum += array[i];
+// }
+// console.log(sum);
+
+
+// program to reverse a string
+const strToReverse = "JavaScript could be even harder, if we only had to read it in reverse"
+
+let reversed = '';
+for (let i = strToReverse.length - 1; i >= 0; i--){
+    reversed += strToReverse[i];
+}
+console.log(reversed)
--- solution.md
+++ solution.md
@@ -1,29 +1,58 @@
 ## Return Negative
+Kata.MakeNegative(1);  // return -1
 
-```js
+function chaToNeg(num)
+{
+    return -Math.abs(num);
+}
+console.log(chaToNeg(1))
 
-```
 
 ## Sum of Positive
 
-```js
+let numArr = [1,-4,7,12];
+let total = 0
 
-```
+for (let i = 0; i < numArr.length; i++){
+    if(numArr[i] > 0){
+        total += numArr[i];
+    }
+  }
+  console.log(total)
+
 
 ## Function 2
 
-```js
+function multy (num1,num2){
+    return num1 * num2
+}
+console.log(multy(2,2))
 
-```
+---------------------not sure if square root is wanted so below is squareroot of a number just in case.
+
+function sqrRoot() {
+    return Math.sqrt(2,5)
+}
+
+console.log(sqrRoot())
 
 ## Sum Arrays
 
-```js
+const array = [2,4,6];
+let sum = 0;
 
-```
+for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+}
+console.log(sum);
+
 
 ## Reversed Strings
 
-```js
+const strToReverse = "JavaScript could be even harder, if we only had to read it in reverse"
 
-```
+let reversed = '';
+for (let i = strToReverse.length - 1; i >= 0; i--){
+    reversed += strToReverse[i];
+}
+console.log(reversed)
